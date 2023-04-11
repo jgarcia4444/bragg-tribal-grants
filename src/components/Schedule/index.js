918919e359f80f0f43cbf5e62521867b2765547e
Jake Garcia <bballwiz4@gmail.com>
2023-04-11 13:47:35 -0700

    Gave the schedule section a title.

--- src/components/Schedule/index.js
+++ src/components/Schedule/index.js
@@ -5,6 +5,8 @@ const Schedule = () => {
 
     return (
         <div id="schedule" className="w-full h-auto">
+            <h2 className="text-blue-950 mb-6 font-bold text-5xl text-center">Schedule</h2>
+            <p>Set up time for a 30 minute call.</p>
             <InlineWidget
                 url='https://calendly.com/jgarciadev4444' 
                 styles={{overflowY: 'hidden', height: '90vh'}}
