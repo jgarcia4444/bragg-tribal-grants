88d7a932887deb10be74d7681feeb06f710fc1d0
Jake Garcia <bballwiz4@gmail.com>
2023-04-18 21:31:02 -0700

    Added in a nav bar component.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Header from './components/Header';
 import OurServices from './components/OurServices';
 import Resources from './components/Resources';
 import Schedule from './components/Schedule';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className=" h-screen">
+      <Navbar />
       <Header />
       <OurServices />
       <Resources />
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from 'react'
+
+const Navbar = () => {
+
+    return (
+        <div className=""></div>
+    )
+}
+
+export default Navbar;
