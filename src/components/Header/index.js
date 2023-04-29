643db3339b9b645e5673f0de3b9076c1c0a7d71b
Jake Garcia <bballwiz4@gmail.com>
2023-04-29 14:11:51 -0700

    Added to the text slide in animation.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -19,7 +19,7 @@ const Header = () => {
     return (
         <div id="header" className={`w-full lg:h-1/2 md:h-1/2 h-2/5 flex flex-col justify-center ${isHidden === true ? "opacity-0" : "opacity-100"} transition-all duration-500`}>
             <HeaderImageCarousel />
-            <div className={`flex flex-col justify-center items-center gap-6 ${textHidden === true ? "translate-x-full" : "tranlate-x-0"} transition-all duration-700`}>
+            <div className={`flex flex-col justify-center items-center gap-6 ${textHidden === true ? "translate-x-full scale-0" : "tranlate-x-0 scale-100"} transition-all duration-1000`}>
                 <div className="headerContent bg-orange-900 bg-opacity-50 p-4 rounded shadow w-3/4 ">
                     <h1 className="mb-4 text-white lg:text-6xl md:text-5xl sm:text-4xl text-4xl font-bold ">Songaa Grants</h1>
                     <p className="text-white lg:text-2xl md:text-xl text-lg font-bold">Bringing federal funds to tribes to supply help where it is needed.</p>
