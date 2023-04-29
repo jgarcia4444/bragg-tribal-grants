86caff70f4158c58ac93ca0f97e9e73bed27694d
Jake Garcia <bballwiz4@gmail.com>
2023-04-29 11:09:30 -0700

    Added the functionality to the navbar component to track the scrollY position of the web page.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,8 +2,41 @@ import React, {useState, useEffect} from 'react'
 
 const Navbar = () => {
 
+    const [showNav, setShowNav] = useState(false);
+    const [lastScrolly, setLastScrolY] = useState(0);
+
+    const controlNavbar = () => {
+        if (typeof window !== 'undefined') {
+            let {scrollY} = window;
+            if (scrollY > 20) {
+                setShowNav(true);
+            } else if (scrollY < 21) {
+                setShowNav(false);
+            } 
+            setLastScrolY(window.scrollY);
+        }
+    }
+
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            window.addEventListener('scroll', controlNavbar);
+            return () => {
+                window.removeEventListener('scroll', controlNavbar)
+            }
+        }
+    },[lastScrolly])
+
     return (
-        <div className=""></div>
+        <div className={`${showNav === true ? "fixed" : "hidden"} top-0 left-0 w-full flex flex-row shadow z-10 bg-white`}>
+            Nav
+            <div className="w-1/3">
+                {/* // logo */}
+            </div>
+            <div className="w-2/3">
+                {/* // cta links */}
+            </div>
+        </div>
     )
 }
 
