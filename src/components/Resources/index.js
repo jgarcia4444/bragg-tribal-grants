2646b26120cd2cd586c64473660fd99cf2518536
Jake Garcia <bballwiz4@gmail.com>
2023-05-04 16:30:45 -0700

    Added in the rest of the content about the ohlone tribe. Made it mobile responsive as well.

--- src/components/Resources/index.js
+++ src/components/Resources/index.js
@@ -19,12 +19,11 @@ const Resources = () => {
     const renderList = () => {
         let listItems = [{text: "Grant Writing", icon: <FiEdit color={"#fff"} size={64}/>}, {text: "Grant Consulting", icon: <FiUser color={"#fff"} size={64}/>}, {text: "Business Planning", icon: <FiFileText color={"#fff"} size={64}/>}];
         return listItems.map((listItem, i) => (
-            <div key={`${i}-${listItem.text}`} className="w-1/3 flex flex-col items-center justify-center h-64">
+            <div key={`${i}-${listItem.text}`} className="md:w-1/3 w-full flex flex-col items-center justify-center h-64">
                 <div className=" w-32 h-32 border-4 border-white rounded-full flex items-center justify-center">
                     {listItem.icon}
                 </div>
                 <p className="text-white text-xl font-bold mt-4">{listItem.text}</p>
-                
             </div>
         ))
     }
@@ -32,9 +31,14 @@ const Resources = () => {
     return (
         <div className="md:px-4 my-16">
             <h2 className="text-blue-950  font-bold text-3xl md:text-5xl text-center md:text-left">What We Do</h2>
-            <div className=" flex flex-row flex-wrap w-full bg-blue-950 rounded py-8">
+            <div className=" flex flex-row flex-wrap w-full bg-blue-950 md:rounded py-8">
                 {/* {renderResources()} */}
                 {renderList()}
+            </div>
+            <div className="w-full flex flex-row flex-wrap items-center justify-center ">
+                <p className="mx-2 md:mx-0 mt-4 md:mt-0 w-full text-blue-950 font-bold text-2xl text-center md:w-1/2"> Representing Ohlone Tribe of Chino Valley</p>
+                <img className="w-full my-2 md:my-0 md:w-1/2" src="https://assets.change.org/photos/1/md/dx/xyMDdxyFgYGtEmz-800x450-noPad.jpg?1595380577" alt="" />
+                <p className="md:w-1/2 mx-2 md:mx-0 w-full text-blue-950 font-bold text-2xl text-center md:mt-2"> Serving Many Califonia Tribes and 14 Tribes in Arizona</p>
             </div>
         </div>
     )
