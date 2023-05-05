79efef5d14abbf4538099854e83b37f8a027bc9f
Jake Garcia <bballwiz4@gmail.com>
2023-05-05 16:02:21 -0700

    Made the list items in the is your tribe dealing with the following little cards that wrap due to flexbox.

--- src/components/OurServices/index.js
+++ src/components/OurServices/index.js
@@ -16,7 +16,7 @@ const OurServices = () => {
 
     const renderLis = () => {
         let lis = ["Lack of Housing", "Lack of Funding", "Lack of Education", "Lack of Health Services", "Lack of Entrepreneurs", "Substance Abuse", "Lack of Income Opportunities"];
-        return lis.map((text, i) => <li className="w-1/2 text-lg md:text-lg font-bold text-white" key={`${i}-${text}`}>{text}</li>)
+        return lis.map((text, i) => <li className={`py-2 px-2 md:px-4 text-base md:text-2xl font-bold mb-4 bg-white rounded shadow-inner text-orange-800 mx-2 transition-all duration-700 hover:shadow hover:scale-105 cursor-pointer`} key={`${i}-${text}`}>{text}</li>)
     }
 
     return (
@@ -24,7 +24,7 @@ const OurServices = () => {
             <h2 className="text-orange-900 font-bold text-3xl md:text-5xl text-center md:text-left">Is Your Tribe Dealing With the Following?</h2>
             <div className="w-full flex flex-row flex-wrap bg-orange-800 items-center justify-center md:rounded">
                 {/* {renderServices()} */}
-                <ul className="list-disc list-inside w-full flex flex-row flex-wrap px-4 py-10">
+                <ul className=" list-inside w-full flex flex-row flex-wrap px-4 py-10 items-center justify-center">
                     {renderLis()}
                 </ul>
             </div>
