e172784a6d879ed8c29fdf9b09b43af5d1932e5d
Jake Garcia <bballwiz4@gmail.com>
2023-05-09 13:05:29 -0700

    Changed the bullet points in the dealing with the following list to have an ornage bg and white text. Made it so the container does not have a bg color.

--- src/components/OurServices/index.js
+++ src/components/OurServices/index.js
@@ -16,13 +16,13 @@ const OurServices = () => {
 
     const renderLis = () => {
         let lis = ["Lack of Housing", "Lack of Funding", "Lack of Education", "Lack of Health Services", "Lack of Entrepreneurs", "Substance Abuse", "Lack of Income Opportunities"];
-        return lis.map((text, i) => <li className={`py-2 px-2 md:px-4 text-base md:text-2xl font-bold mb-4 bg-white rounded shadow-inner text-orange-800 mx-2 transition-all duration-700 hover:shadow hover:scale-105 cursor-pointer`} key={`${i}-${text}`}>{text}</li>)
+        return lis.map((text, i) => <li className={`py-2 px-2 md:px-4 text-base md:text-2xl font-bold mb-4 text-white rounded shadow-xl bg-orange-800 mx-2 transition-all duration-700 hover:shadow hover:scale-105 cursor-pointer`} key={`${i}-${text}`}>{text}</li>)
     }
 
     return (
         <div className="w-full mx-auto flex justify-center flex-col lg:my-20 mt-28 md:mt-12 md:px-4">
             <h2 className="text-orange-900 font-bold text-3xl md:text-5xl text-center md:text-left">Is Your Tribe Dealing With the Following?</h2>
-            <div className="w-full flex flex-row flex-wrap bg-orange-800 items-center justify-center md:rounded">
+            <div className="w-full flex flex-row flex-wrap items-center justify-center md:rounded">
                 {/* {renderServices()} */}
                 <ul className=" list-inside w-full flex flex-row flex-wrap px-4 py-10 items-center justify-center">
                     {renderLis()}
